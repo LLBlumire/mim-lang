e300f103c4896b1faadbadf001ae00cb20b1a1ef
Lucille Blumire <llblumire@gmail.com>
2018-02-19 10:29:59 +0000

    Begin rewrite of MimC using new architecture specification.

---
+++ implementations/mimc/src/asm/mode.rs
@@ -0,0 +1,5 @@
+pub enum Mode {
+    TypeDef = 0,
+    Data = 1,
+    Main = 2,
+}
--- implementations/mimc/src/main.rs
+++ implementations/mimc/src/main.rs
@@ -2,46 +2,65 @@
 extern crate clap;
 
 extern crate byteorder;
-extern crate mim_lang_core as core;
 
-mod asm;
-
-use clap::App;
 use clap::Arg;
+use clap::App;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+
+mod asm;
 
 fn main() {
     let matches = App::new("Mim Compiler")
         .version(crate_version!())
         .author(crate_authors!())
-        .about("Compiler Mim and MimASM")
-        .arg(
-            Arg::with_name("asm")
-                .short("a")
-                .long("asm")
-                .value_name("FILE")
-                .takes_value(true)
-                .multiple(true),
-        )
-        .arg(
-            Arg::with_name("out")
-            .short("o")
-            .long("out")
+        .arg(Arg::with_name("assembler")
+            .short("a")
+            .long("assembler")
             .value_name("FILE")
+            .help("A MimASM file to compile.")
+            .multiple(true)
+            .takes_value(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("FILE")
+            .help("The file to output too.")
             .takes_value(true)
-            .required(true)
-        )
+            .required(true))
         .get_matches();
-    
-    let out_path = matches.value_of("out").unwrap();
-    let mut out_file = File::create(out_path).unwrap();
 
-    for asm_path in matches.values_of("asm").unwrap() {
-        let mut asm_file = File::open(&asm_path).unwrap();
-        let mut buf = String::new();
-        asm_file.read_to_string(&mut buf).unwrap();
-        out_file.write(&asm::compile(&buf)).unwrap();
+    let output_file = matches.value_of("output").unwrap();
+
+    let mut output: Vec<u8> = Vec::new();
+    for asm_file in matches.values_of("assembler").into_iter().flat_map(|n| n) {
+        let file_content = {
+            if let Ok(mut file_handle) = File::open(asm_file) {
+                let mut buf = String::new();
+                if let Ok(_) = file_handle.read_to_string(&mut buf) {
+                } else {
+                    println!("Unable to Read File: `{}`", asm_file);
+                }
+                buf
+            } else {
+                println!("Unknown Input Assembler: `{}`", asm_file);
+                continue;
+            }
+        };
+        println!("Building Input Assembler: `{}`", asm_file);
+        output.append(&mut asm::compile(&file_content));
     }
+    
+
+    if let Ok(mut file_handle) = File::create(output_file) {
+        if let Ok(_) = file_handle.write_all(&output) {
+            println!("Writing to File: `{}`", output_file);
+        } else {
+            println!("Failed to Write: `{}`", output_file);
+        }
+    } else {
+        println!("Failed to Create: `{}`", output_file);
+    }
+
 }
